421d270de925262ea447f114f4c441cafa7a55ea
Andrey Bolanos Sandoval <andreybs11@gmail.com>
2016-08-27 17:30:20 -0600

    Working blood type selector

--- www/src/components/Authenticated/Onboarding/BloodTypeSelector.vue
+++ www/src/components/Authenticated/Onboarding/BloodTypeSelector.vue
@@ -1,6 +1,18 @@
 <template>
-    <div v-for="blood in bloodTypes">
-        <div>{{blood.type}}</div>
+    <div class="row">
+        <h5 class="center white-text">¿Cuál es tu tipo de sangre?</h5>
+        <form class="col s12">
+            <div class="row">
+                <div class="input-field col s12">
+                    <select v-model="bloodType">
+                        <option v-for="blood in bloodTypes" v-bind:value="blood.key">{{blood.type}}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row center">
+                <button v-on:click="submit" class="btn waves-effect waves-light red lighten-2" type="submit" name="action">Siguiente</button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -15,15 +27,29 @@
             { type : 'AB-', key : 'abn' },
             { type : 'O+', key : 'op' },
             { type : 'O-', key : 'on' },
-            { type : '?', key : 'null' }                                                                                    
-        ];        
+            { type : '?', key : 'null' }
+        ];
     };
 
     module.exports = {
         data : function() {
             return {
+                bloodType: 'ap',
                 bloodTypes : getBloodTypes()
             };
+        },
+        methods : {
+            submit : function() {
+                localStorage.setItem('blood_type', this.bloodType);
+                this.$route.router.go('/onboarding/age');
+            }
+        },
+        ready : function() {
+            var self = this;
+            $('select').material_select();
+            $('select').change(function(){
+                self.$set('bloodType', $('select').val());
+            });
         }
     };
 </script>
